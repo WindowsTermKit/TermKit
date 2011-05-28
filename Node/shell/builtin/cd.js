25a47dacb00be084677c95f1252eecfae23621c3
James Rhodes <jrhodes@redpointsoftware.com.au>
2011-05-29 00:43:06 +1000

    Updated 'cd' command to prevent user navigating above the drive level in Windows.

--- Node/shell/builtin/cd.js
+++ Node/shell/builtin/cd.js
@@ -10,13 +10,31 @@ exports.main = function (tokens, pipes, exit) {
     out.print('Usage: cd [dir]');
     return exit(false);
   }
-  var path = tokens[1] || '~';
+  var path = null;
+  
+  // Check what default path we should change to if no arguments
+  // were provided.
+  var oldPath = process.cwd();
+  var wasWindows = false;
+  if (oldPath.substring(0, "/cygdrive/".length) == "/cygdrive/")
+  {
+    wasWindows = true;
+    path = tokens[1] || oldPath.substring(0, "/cygdrive/".length + 1);
+  }
+  else
+    path = tokens[1] || '~';
   
   // Complete path
   expandPath(path, function (path) {
     // Try to change working dir.
     try {
       process.chdir(path);
+      
+      // Ensure that in Windows we remain in a drive at all times.
+      if (wasWindows && process.cwd().substring(0, "/cygdrive/".length) != "/cygdrive/") {
+        out.print('You can\'t move higher than the top-level directory.');
+        process.chdir(oldPath.substring(0, "/cygdrive/".length + 1));
+      }
     }
     catch (error) {
       out.print(error.message + ' (' + path + ')');
